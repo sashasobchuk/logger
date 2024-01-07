86c879737b48c787e94f4a66cd2a9d41096a69fe
sashasobchuk <sasha.sobchuk18@gmail.com>
2024-01-07 12:00:45 +0200

    changeOnRebuild done

--- src/logger/try-insert-logger-row-to-DB.ts
+++ src/logger/try-insert-logger-row-to-DB.ts
@@ -6,26 +6,30 @@ import { memory_addLogger } from "./memory_addLogger";
 import { LoggerType } from "./types";
 import { memory_updateLogger } from "./memory_update-logger";
 
-export async function tryInsertLoggerRowToDB(loggerEntity: LoggerEntity,loggers:LoggerType[]) {
+export async function tryInsertLoggerRowToDB(logger_toDB: LoggerEntity,loggers:LoggerType[]) {
   /** при перезагрузці апдейтим або створюємо нові логери в базі*/
-  const {name} = loggerEntity
+  const {name} = logger_toDB
   return checkLoggerInDB(name)
-    .then((logger) => {
+    .then((db_logger) => {
       //todo після бази оновляти чи створювати в памяті
-      if (!logger) {
+      if (!db_logger) {
         /** якщо нема то створюжмо в базі*/
-       return  db_createLogger(loggerEntity)
+       return  db_createLogger(logger_toDB)
           .then((data)=>{
             /** і додаємо його в памяті*/
-          return   memory_addLogger(logger,loggers)
+          return   memory_addLogger(db_logger,loggers)
           })
       } else {
         //todo можна буде продумати в яких випадках обновляти
-        /** якщо є апдейтим його в базі*/
-        return db_updateLogger(loggerEntity)
+        /** якщо є і потрібно оновити апдейтим його в базі*/
+        if(!db_logger.changeOnRebuild){
+          /** пропистити оновленя даних на ребілд (буде залишатись апдейт з бази)*/
+          return
+        }
+        return db_updateLogger(logger_toDB)
           .then(data=>{
             /** і апдейтим його в памяті*/
-           return  memory_updateLogger(logger,loggers)
+           return  memory_updateLogger(db_logger,loggers)
           })
       }
     }).catch(e=>{
